3a7265bbb8bfb44faba16b1fc0998cc3e79a1d91
Riley <riley.childs@yahoo.com>
2022-05-08 15:06:05 -0400

    Created mobile nav menu toggle

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,12 +1,26 @@
 const images = document.querySelectorAll("img");
 
-if(screen.width <= 390){
-Object.keys(images).forEach((image) => {
+if (screen.width <= 390) {
+  Object.keys(images).forEach((image) => {
     let url = images[image].src;
-    const match = /desktop/.test(url)
-    if (match ===true) {
-        const mobileURL = url.replace(/desktop/, "mobile")
-        images[image].src = mobileURL
+    const match = /desktop/.test(url);
+    if (match === true) {
+      const mobileURL = url.replace(/desktop/, "mobile");
+      images[image].src = mobileURL;
     }
-})
+  });
 }
+
+const mobileMenu = document.querySelector("#topNav_mobile_menu");
+const mobileMenuToggle = document.querySelector("#topNav_mobile--open");
+
+mobileMenu.style.display = "none";
+
+let toggleMenu = () => {
+    if (mobileMenu.style.display === "block") {
+        mobileMenu.style.display = "none";
+      } else {
+        mobileMenu.style.display = "block";
+      }
+};
+
