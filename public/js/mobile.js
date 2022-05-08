a505f017550bbfba142aa9a62823422ede7f2dd1
Riley <riley.childs@yahoo.com>
2022-05-08 10:21:29 -0400

    Used js and RegEx to change most img directories to mobile

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -1,3 +1,12 @@
-if(screen.width <= 390){
+const images = document.querySelectorAll("img");
 
+if(screen.width <= 390){
+Object.keys(images).forEach((image) => {
+    let url = images[image].src;
+    const match = /desktop/.test(url)
+    if (match ===true) {
+        const mobileURL = url.replace(/desktop/, "mobile")
+        images[image].src = mobileURL
+    }
+})
 }
